9c60f0d936aa26dacf35d483eb61673b6f091e0c
Mohammadreza Farhadi <farhadimrf@gmail.com>
2024-02-20 23:06:30 +0330

    feat: improve chat section and bot auto reply

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
    return (
       <html lang="en">
          <body className={inter.className}>
-            <Chatbot direction="bottom-right" />
+            <Chatbot direction="bottom-left" />
          </body>
       </html>
    );
--- components/Chatbot.tsx
+++ components/Chatbot.tsx
@@ -1,15 +1,58 @@
 "use client";
 import { ChevronDown, MessageSquareText, SendHorizontal, UserRound, X } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 type ChatbotProps = {
    direction: "bottom-right" | "bottom-left";
 };
+type ChatMessage = {
+   type: "user" | "bot";
+   text: string;
+};
+
+type FormData = {
+   message: string;
+};
 
 const Chatbot = ({ direction }: ChatbotProps) => {
    const [isOpen, setIsOpen] = useState(true);
-   const chatPosition = direction === "bottom-right" ? "right-1 sm:right-3" : "left-3 sm:left-3";
+   const [chatMessages, setChatMessages] = useState<ChatMessage[]>([]);
+   const chatPosition = direction === "bottom-right" ? "right-1 sm:right-3" : "left-1 sm:left-3";
    const chatHeight = isOpen ? "h-[75vh] sm:max-h-[650px]" : "h-0";
+
+   const { register, handleSubmit, setValue } = useForm<FormData>();
+   console.log("iam renderefe");
+   const generateRandomBotResponse = () => {
+      const responses = [
+         "I'm here to help!",
+         "How can I assist you today?",
+         "Ask me anything!",
+         "Let me know what you need.",
+      ];
+      const randomIndex = Math.floor(Math.random() * responses.length);
+      return responses[randomIndex];
+   };
+   useEffect(() => {
+      const botMessage = generateRandomBotResponse();
+      setChatMessages([{ type: "bot", text: "Hi, How can i help you?" }]);
+   }, []);
+
+   const onSubmit: SubmitHandler<FormData> = (data) => {
+      const userMessage = data.message.trim();
+
+      if (userMessage !== "") {
+         const botMessage = generateRandomBotResponse();
+
+         setChatMessages((prevMessages) => [
+            ...prevMessages,
+            { type: "user", text: userMessage },
+            { type: "bot", text: botMessage },
+         ]);
+
+         setValue("message", "");
+      }
+   };
 
    return (
       <>
@@ -46,27 +89,37 @@ const Chatbot = ({ direction }: ChatbotProps) => {
                   />
                </div>
 
-               <div className="h-full w-full flex flex-col text-sm px-1 pt-2">
-                  <div className="bg-white text-black self-start p-2 m-2 rounded-xl border max-w-[60%]">
-                     Hello! How can I help you?
-                  </div>
-                  <div className="bg-[#006ffd] text-white self-end p-2 m-2 rounded-xl max-w-[60%]">
-                     Im here to assist you.
-                  </div>
+               <div className="h-full w-full flex flex-col text-sm px-1 pt-2 overflow-auto">
+                  {chatMessages.map((message, index) => (
+                     <div
+                        key={index}
+                        className={`bg-${
+                           message.type === "bot"
+                              ? "white text-black self-start"
+                              : "[#006ffd] text-white self-end"
+                        } p-2 m-2 rounded-3xl px-3 border max-w-[70%]`}
+                     >
+                        {message.text}
+                     </div>
+                  ))}
                </div>
 
-               <div className="flex pb-2 px-2 items-center w-full gap-1">
+               <form
+                  onSubmit={handleSubmit(onSubmit)}
+                  className="flex pb-2 px-2 items-center w-full gap-1"
+               >
                   <div className="border flex-1 rounded-3xl p-1.5 ">
                      <input
+                        {...register("message")}
                         type="text"
                         placeholder="Type your message..."
                         className="w-full bg-transparent focus:outline-none placeholder:text-sm placeholder:ps-1"
                      />
                   </div>
-                  <div className="rounded-full p-2 rotate-180 bg-[#006ffd]">
+                  <button type="submit" className="rounded-full p-2 rotate-180 bg-[#006ffd]">
                      <SendHorizontal className="h-5 w-5 text-white" />
-                  </div>
-               </div>
+                  </button>
+               </form>
             </div>
          )}
       </>
